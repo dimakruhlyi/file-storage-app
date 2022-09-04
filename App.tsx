7dd6d80a0a549e1bb9b47523a489bc130251682c
dimakruhlyi <dimakruhly@gmail.com>
2022-09-04 14:55:29 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+
+const App = () => {
+ 
+
+  return (
+    <View>
+      <Text>Storage Android app</Text>
+    </View>
+  );
+};
+
+
+export default App;
